57a4107e92439091661b067535ab85c752e41d66
shenmz084122 <shenmz@aliyun.com>
2025-03-03 21:23:28 +0800

    浪淘测试提交

--- xgo/types/pbmodel/sync_job.validator.pb.go
+++ xgo/types/pbmodel/sync_job.validator.pb.go
@@ -150,8 +150,8 @@ func (this *SyncJob) _xxx_xxx_Validator_Validate_source_type() error {
 	if !(this.SourceType > 0) {
 		return protovalidator.FieldError1("SyncJob", "the value of field 'source_type' must be greater than '0'", protovalidator.Int32ToString(int32(this.SourceType)))
 	}
-	if !(this.SourceType <= 17) {
-		return protovalidator.FieldError1("SyncJob", "the value of field 'source_type' must be less than or equal to '17'", protovalidator.Int32ToString(int32(this.SourceType)))
+	if !(this.SourceType <= 19) {
+		return protovalidator.FieldError1("SyncJob", "the value of field 'source_type' must be less than or equal to '19'", protovalidator.Int32ToString(int32(this.SourceType)))
 	}
 	if !(_xxx_xxx_Validator_SyncJob_InEnums_SourceType[this.SourceType]) {
 		return protovalidator.FieldError1("SyncJob", "the value of field 'source_type' must in enums of '[0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19]'", protovalidator.Int32ToString(int32(this.SourceType)))
